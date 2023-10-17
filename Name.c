c79f8a2c0607d27b1c188e22e6f13569cc1c3fd7
Alphonce Kipng'eno Mutai <mutaikalfonce@gmail.com>
2023-10-18 00:09:02 +0300

    program that takes an input and print it out

---
+++ Name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	char name[50];
+
+	/*program to print out my name */
+	printf("What is your name?");
+	scanf("%s", name);
+	printf("My name is :%s\n", name);
+	return (0);
+}
